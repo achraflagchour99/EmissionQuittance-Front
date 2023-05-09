732ab263289a5928db6ec1143dafb0bb44a9e162
achraflagchour99 <achraflagchour1999@gmail.com>
2023-05-09 14:18:34 +0100

    table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import "./App.css";
 import React from "react";
-
-
 import { MainContent } from "./layout/MainContent";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from "./pages/Dashboard";
@@ -10,7 +8,7 @@ import SearchPolice from "./components/Forms/SearchPolice";
 import Searchpolicepage from "./pages/Searchpolicepage";
 import ConsultPolicePage from "./pages/ConsultPolicePage";
 import AddPolice from "./components/Forms/AddPolice";
-
+import Example from "./components/Forms/TS";
 const mdTheme = createTheme();
 
 function App() {
@@ -20,13 +18,13 @@ function App() {
   };
   return (
 
-        
+
         <BrowserRouter>
             <Routes>
                 <Route>
                     <Route path='/' element={<MainContent />}>
                         <Route index                         element={<Dashboard />} />
-                        {/*<Route path='test'      element={<Example />} />*/}
+                        <Route path='test'       element={<Example/>} />
                         <Route path='police-search'      element={<Searchpolicepage />} />
                         <Route path='police-add'      element={<AddPolice />} />
                         <Route path="/consult-page/:codePolice" element={<ConsultPolicePage/>} />
