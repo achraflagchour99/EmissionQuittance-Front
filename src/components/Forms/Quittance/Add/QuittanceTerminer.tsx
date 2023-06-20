9bff193441e7fc0da79550cfeb0cfda89a5ffab8
R0001324 <youssef.lahbabta@gmail.com>
2023-06-20 17:34:59 +0200

    tableau Somme  garantie done stepperQuittance done

---
+++ src/components/Forms/Quittance/Add/QuittanceTerminer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SuccessMessage() {
+  return (
+    <div style={{ backgroundColor: 'green', color: 'white', padding: '10px' }}>
+      Félicitations ! L'opération a été effectuée avec succès.
+    </div>
+  );
+}
+
+export default SuccessMessage;
---
+++ src/components/Forms/Quittance/recoil/atoms.tsx
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const idCodePoliceState = atom<string>({
+  key: 'idCodePoliceState',
+  default: ''
+});
